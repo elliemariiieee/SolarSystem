9246299402eddbf94797d405fb73a139e531a794
elliemariiieee <399384@colum.edu>
2023-10-26 02:23:34 -0500

    finalized solar system

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,7 @@
 import * as three from "three";
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import background from '../img/background.jpg'; //Photo by <a href="https://unsplash.com/@jeremythomasphoto?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Jeremy Thomas</a> on <a href="https://unsplash.com/photos/blue-and-purple-galaxy-digital-wallpaper-E0AHdsENmDg?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
-
+import background from '../img/background.jpg'; //Photo by <a href="https://unsplash.com/@von_co?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Ivana Cajina</a> on <a href="https://unsplash.com/photos/milky-way-asuyh-_ZX54?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
 var height = window.innerHeight;
 var width = window.innerWidth;
 
@@ -21,7 +20,7 @@ document.body.appendChild(renderer.domElement);
 
 const camera = new three.PerspectiveCamera(45, width/height, 0.1, 1000);
 const orbit = new OrbitControls(camera, renderer.domElement);
-camera.position.set(-10,30,60);
+camera.position.set(-100,100,100);
 orbit.update();
 
 window.addEventListener('resize', () => {
@@ -32,18 +31,20 @@ window.addEventListener('resize', () => {
     camera.updateProjectionMatrix();
 });
 
-//lighting
-const ambientLight = new three.AmbientLight(0xFFFFFF);
-scene.add(ambientLight);
+//sun lighting help from https://threejs.org/examples/#webgl_lights_pointlights
+const sun = new three.PointLight(0xffffff, 10000);
+sun.castShadow = true;
+sun.shadow.camera.near = 1;
+sun.shadow.camera.far = 100000;
+const sunGeo = new three.SphereGeometry(10,10,10);
+const sunMat = new three.MeshBasicMaterial({color:0xffd400});
+const sunSphere = new three.Mesh(sunGeo, sunMat);
+sun.add(sunSphere);
+scene.add(sun);
+const lastZPos = sunSphere.geometry.attributes.position.array.length-1;
 
 //solar system
-const sunGeo = new three.SphereGeometry(10,20,20);
-const sunMat = new three.MeshLambertMaterial({color:0xffd400});
-const sun = new three.Mesh(sunGeo, sunMat);
-scene.add(sun);
-sun.castShadow = true;
-
-const mercuryGeo = new three.SphereGeometry(2,5,5);
+const mercuryGeo = new three.SphereGeometry(2,10,10);
 const mercuryMat = new three.MeshLambertMaterial({color:0xd3d3d3});
 const mercury = new three.Mesh(mercuryGeo,mercuryMat);
 scene.add(mercury);
@@ -53,7 +54,7 @@ mercuryObj.add(mercury);
 scene.add(mercuryObj);
 mercury.position.x = sun.position.x + 15;
 
-const venusGeo = new three.SphereGeometry(3, 6, 6);
+const venusGeo = new three.SphereGeometry(3, 10, 10);
 const venusMat = new three.MeshLambertMaterial({color: 0xfaf2df});
 const venus = new three.Mesh(venusGeo, venusMat);
 scene.add(venus);
@@ -73,7 +74,7 @@ earthObj.add(earth);
 scene.add(earthObj);
 earth.position.x = sun.position.x + 40;
 
-const marsGeo = new three.SphereGeometry(4, 8, 8);
+const marsGeo = new three.SphereGeometry(4, 10, 10);
 const marsMat = new three.MeshLambertMaterial({color: 0xf0e7e7});
 const mars = new three.Mesh(marsGeo, marsMat);
 scene.add(mars);
@@ -100,8 +101,28 @@ scene.add(saturn);
 saturn.receiveShadow = true;
 const saturnObj = new three.Object3D();
 saturnObj.add(saturn);
+const ringGeo = new three.RingGeometry(10, 13, 20);
+const ringMat = new three.MeshLambertMaterial({color: 0xd8ae6d, side: three.DoubleSide});
+const ringMat2 = new three.MeshLambertMaterial({color: 0xffee1ab, side: three.DoubleSide});
+const ring1 = new three.Mesh(ringGeo, ringMat);
+const ring2 = new three.Mesh(ringGeo, ringMat2);
+const ring3 = new three.Mesh(ringGeo, ringMat);
+ring1.receiveShadow = true;
+ring2.receiveShadow = true;
+ring3.receiveShadow = true;
+ring2.scale.set(1.25, 1.25, 1.25);
+ring3.scale.set(1.5, 1.5, 1.5);
+saturnObj.add(ring1);
+saturnObj.add(ring2);
+saturnObj.add(ring3);
 scene.add(saturnObj);
 saturn.position.x = sun.position.x + 100;
+ring1.position.set(saturn.position.x, saturn.position.y, saturn.position.z);
+ring2.position.set(saturn.position.x, saturn.position.y, saturn.position.z);
+ring3.position.set(saturn.position.x, saturn.position.y, saturn.position.z);
+ring1.rotateX(Math.PI/2);
+ring2.rotateX(Math.PI/2);
+ring3.rotateX(Math.PI/2);
 
 const uranusGeo = new three.SphereGeometry(7, 14, 14);
 const uranusMat = new three.MeshLambertMaterial({color: 0xace5ee});
@@ -123,7 +144,7 @@ neptuneObj.add(neptune);
 scene.add(neptuneObj);
 neptune.position.x = sun.position.x + 140;
 
-const plutoGeo = new three.SphereGeometry(2, 4, 4);
+const plutoGeo = new three.SphereGeometry(2, 10, 10);
 const plutoMat = new three.MeshLambertMaterial({color: 0xf7bb7b});
 const pluto = new three.Mesh(plutoGeo, plutoMat);
 scene.add(pluto);
@@ -132,6 +153,21 @@ const plutoObj = new three.Object3D();
 plutoObj.add(pluto);
 scene.add(plutoObj);
 pluto.position.x = sun.position.x + 155;
+
+const starURL = new URL('../assets/star.glb', import.meta.url);//"Gold Star" (https://skfb.ly/6woS9) by Oleksandr Pelypenko is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
+assetLoader.load(
+    starURL.href,
+    function(gltf){
+        const model = gltf.scene;
+        scene.add(model);
+        model.position.set(10, 20, 10);
+        model.scale.set(10, 10, 10);
+    },
+    undefined,
+    function(error){
+        console.error(error);
+    }
+)
 
 function animate(time){
     sun.rotation.x = time/1000000;
@@ -161,6 +197,13 @@ function animate(time){
     uranusObj.rotation.y = time/16000;
     neptuneObj.rotation.y = time/32000;
     plutoObj.rotation.y = time/64000;
+
+    for (let i = 0; i < sunSphere.geometry.attributes.position.array.length; i+=15){
+        sunSphere.geometry.attributes.position.array[i] = 3*Math.random();
+        sunSphere.geometry.attributes.position.array[lastZPos] = 5*Math.random();
+    }
+    sunSphere.geometry.attributes.position.needsUpdate = true;
+
     renderer.render(scene,camera);
 }
 renderer.setAnimationLoop(animate);
